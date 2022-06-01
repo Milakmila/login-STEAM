de7694a7fdb61083b4abde6a9a98402730083fe7
milakmila <camilaleja.cipa@gmail.com>
2022-06-01 08:32:08 -0500

    Update of changes

--- src/index.js
+++ src/index.js
@@ -12,12 +12,8 @@ import ScoreS from './students/Score';
 import ScoreT from './teachers/scoreTeacher'
 import Recursos from './pages/recursos';
 import Error404 from './pages/Error404';
-<<<<<<< HEAD
-import Courses from './pages/Courses';
-
-=======
 import Login from "./pages/Login";
->>>>>>> 8712c3d6a8c8627b63b73420c305467a16aff2fb
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -32,14 +28,11 @@ root.render(
     <Route path='/home' element={<Navigate replace to={"/"} />}></Route>
     <Route path='*' element={<Error404 />}></Route>
     <Route path='/courses' element={<Courses />}></Route>
-<<<<<<< HEAD
     <Route path='/courses/:id' element={<Course />}></Route>
-   
-=======
     <Route path='/course/:id' element={<Course />}></Route>
     <Route path='/login' element={<Login />}></Route>
   
->>>>>>> 8712c3d6a8c8627b63b73420c305467a16aff2fb
+
     </Routes>
   </BrowserRouter>
  
--- src/pages/Course.js
+++ src/pages/Course.js
@@ -1,18 +1,14 @@
 import Header from "../components/Header/Header"
-<<<<<<< HEAD
+
 import {Link, useParams} from "react-router-dom";
 import {getCourse} from "../courses";
 import '../components/style/modal.css'
-=======
-import Footer from "../components/Footer/Footer"
-import {useParams} from "react-router-dom";
-import {getCourse} from "../courses";
->>>>>>> 8712c3d6a8c8627b63b73420c305467a16aff2fb
+import Footer from "../components/Footer/Footer";
+
 
 
 function Course(){
     const params=useParams();
-<<<<<<< HEAD
     const  course=getCourse(parseInt(params.id));
     return(
         <div>
@@ -26,30 +22,10 @@ function Course(){
                 <h1>Detalle de </h1>
                 <h3>{course.trabajo}</h3>
                 <p>{course.descripcion}</p>
-=======
-    const  cours=getCourse(parseInt(params.id));
-
-    return(
-        <div>
-            {params.id}
-            <Header />
-            <div>
-                IMAGEN como array
-            </div>
-            <div className="card-deliver">
-                <h3>{cours.trabajo}</h3>
-                <p><strong>Descripción: </strong>{cours.descripción}</p>
->>>>>>> 8712c3d6a8c8627b63b73420c305467a16aff2fb
                 <div class="icons" >
                     <a href="#modal" class="cta"><i class="fa-solid fa-paperclip" ></i></a>
                     <a href="#modal" class="cta"><i class="fa-solid fa-comment"></i></a>
                 </div>
-<<<<<<< HEAD
-               
-=======
-                
-        
->>>>>>> 8712c3d6a8c8627b63b73420c305467a16aff2fb
             </div>
             <section id="modal" class="modal">
         
@@ -61,18 +37,6 @@ function Course(){
                   <input type="file" id="file" name="file" multiple></input>
                 </div>
         
-<<<<<<< HEAD
-                <textarea name="textarea" className="borde" rows="10" cols="50">Hola profesor...
-            
-                </textarea>
-               </form>
-               <Link to="/courses">
-                <button class="btn-green">
-                    <i class="fa-solid fa-user"></i>
-                    <h5>Exportar</h5>
-                </button>
-            </Link>
-=======
                 <textarea name="textarea" rows="10" cols="50">Hola profesor...
             
                 </textarea>
@@ -80,19 +44,13 @@ function Course(){
                </form>
 
                
->>>>>>> 8712c3d6a8c8627b63b73420c305467a16aff2fb
 
 
 
         </div>
         </section>
-<<<<<<< HEAD
 
-
-=======
->>>>>>> 8712c3d6a8c8627b63b73420c305467a16aff2fb
-
-            <Footer />
+        <Footer />
 
         </div>
     )
