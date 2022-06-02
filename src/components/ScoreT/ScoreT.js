b8adf161cf38582451820a6ed3abc39f49998ea9
milakmila <camilaleja.cipa@gmail.com>
2022-06-02 15:30:31 -0500

    use of test for databse in json

--- src/components/ScoreT/ScoreT.js
+++ src/components/ScoreT/ScoreT.js
@@ -7,7 +7,7 @@ function ScoreT(){
     <section>
         <BotonI />
         <div class="botons">
-            <Link to="/score-teacher">
+            <Link to="/score-students">
                 <BotonV />
             </Link>
         </div>
@@ -22,21 +22,12 @@ function ScoreT(){
         </div>
 
 
-            <table className="table">
-                <tr className="rowprincipal-teacher"> 
-                    <th class="student-teacher"> Estudiante:</th>
-                    <th class="email-teacher">Correo:</th>
-                    <th class="workshop-teacher">Trabajos:</th>
-                    <th class="score-teacher">Puntaje:</th>
-                    <th class="average-teacher">Promedio:</th>
-                </tr>
-                <tr className="row-teacher"> 
-                    <td class="student"> 
-                        <a href="#modal" class="estudiantenombre"><i class="fa-solid fa-pencil"></i></a>
-                        Arias Catalina
-                    </td>
+                    
+                  
 
-                        <article id="modal" className="modal">
+                    
+                 
+                    <article id="modal" className="modal">
                             <div class="modalContainer2">
                                 <div class="imagenestudiante">
                                 <i class="fa-solid fa-circle-user userss"></i>
@@ -69,333 +60,7 @@ function ScoreT(){
                             
                         
                         </article>
-                    
-                    <td class="email">catalinarias@colegiosanpatricio.com</td>
-                    <td class="workshop">
-                        
-                        <div class="worskhop-two">
-                        <a href="#modal2">
-                        <i class="fa-solid fa-paperclip"></i>Trabajo:Lorem ipsum
-                        </a>
-                        </div>
-                    <div class="worskshop-three">
-                            <a href="#modal2">
-                                <i class="fa-solid fa-paperclip"></i>Trabajo:Lorem ipsum
-                                </a>
-                        </div>
-                        <div class="workshop-four">
-                            <a href="#modal2">
-                                <i class="fa-solid fa-paperclip"></i>Trabajo:Lorem ipsum
-                                </a>
-                        </div>
-                    </td>
-                    <td class="score-students">
-                            <div>
-                            10
-                            </div>
-         
-                            <div>
-                            10
-                            </div>
-                            <div>
-                            10
-                            </div>
-                    </td>
-                    <td class="average">98%</td>
-                    </tr>
-                    <tr className="row-teacher"> 
-                    <td class="student"> 
-                        <a href="#modal" class="estudiantenombre"><i class="fa-solid fa-pencil"></i></a>
-                        Arias Catalina
-                    </td>
-
-                        <article id="modal" className="modal">
-                            <div class="modalContainer2">
-                                <div class="imagenestudiante">
-                                <i class="fa-solid fa-circle-user userss"></i>
-                                </div>
-                                <h2 class="nombre">Arias Catalina</h2>
-                                <h3 class="grado">9 Grado</h3>
-                                <div class="lista">
-                                <h4>Trabajos:</h4>
-                                <ul>
-                                    <li>Nombre trabajo1</li>
-                                    <li>Nombre trabajo2</li>
-                                    <li>Nombre trabajo3</li>
-                                </ul>
-                        
-                                </div>
-                                <div class="lista2">
-                                    <h4>notas:</h4>
-                                    <ul>
-                                        <li>7</li>
-                                        <li>5</li>
-                                        <li>10</li>
-                                    </ul>
-                                </div>
-                                
-
-                                <a href="">
-                                    <button class="btn-volver">Volver</button>
-                                    </a>
-                            </div>
-                            
-                        
-                        </article>
-                    
-                    <td class="email">catalinarias@colegiosanpatricio.com</td>
-                    <td class="workshop">
-                        
-                        <div class="worskhop-two">
-                        <a href="#modal2">
-                        <i class="fa-solid fa-paperclip"></i>Trabajo:Lorem ipsum
-                        </a>
-                        </div>
-                    <div class="worskshop-three">
-                            <a href="#modal2">
-                                <i class="fa-solid fa-paperclip"></i>Trabajo:Lorem ipsum
-                                </a>
-                        </div>
-                        <div class="workshop-four">
-                            <a href="#modal2">
-                                <i class="fa-solid fa-paperclip"></i>Trabajo:Lorem ipsum
-                                </a>
-                        </div>
-                    </td>
-                    <td class="score-students">
-                            <div>
-                            10
-                            </div>
-         
-                            <div>
-                            10
-                            </div>
-                            <div>
-                            10
-                            </div>
-                    </td>
-                    <td class="average">98%</td>
-                    </tr>
-                    <tr className="row-teacher"> 
-                    <td class="student"> 
-                        <a href="#modal" class="estudiantenombre"><i class="fa-solid fa-pencil"></i></a>
-                        Arias Catalina
-                    </td>
-
-                        <article id="modal" className="modal">
-                            <div class="modalContainer2">
-                                <div class="imagenestudiante">
-                                <i class="fa-solid fa-circle-user userss"></i>
-                                </div>
-                                <h2 class="nombre">Arias Catalina</h2>
-                                <h3 class="grado">9 Grado</h3>
-                                <div class="lista">
-                                <h4>Trabajos:</h4>
-                                <ul>
-                                    <li>Nombre trabajo1</li>
-                                    <li>Nombre trabajo2</li>
-                                    <li>Nombre trabajo3</li>
-                                </ul>
-                        
-                                </div>
-                                <div class="lista2">
-                                    <h4>notas:</h4>
-                                    <ul>
-                                        <li>7</li>
-                                        <li>5</li>
-                                        <li>10</li>
-                                    </ul>
-                                </div>
-                                
-
-                                <a href="">
-                                    <button class="btn-volver">Volver</button>
-                                    </a>
-                            </div>
-                            
-                        
-                        </article>
-                    
-                    <td class="email">catalinarias@colegiosanpatricio.com</td>
-                    <td class="workshop">
-                        
-                        <div class="worskhop-two">
-                        <a href="#modal2">
-                        <i class="fa-solid fa-paperclip"></i>Trabajo:Lorem ipsum
-                        </a>
-                        </div>
-                    <div class="worskshop-three">
-                            <a href="#modal2">
-                                <i class="fa-solid fa-paperclip"></i>Trabajo:Lorem ipsum
-                                </a>
-                        </div>
-                        <div class="workshop-four">
-                            <a href="#modal2">
-                                <i class="fa-solid fa-paperclip"></i>Trabajo:Lorem ipsum
-                                </a>
-                        </div>
-                    </td>
-                    <td class="score-students">
-                            <div>
-                            10
-                            </div>
-         
-                            <div>
-                            10
-                            </div>
-                            <div>
-                            10
-                            </div>
-                    </td>
-                    <td class="average">98%</td>
-                    </tr>
-                    <tr className="row-teacher"> 
-                    <td class="student"> 
-                        <a href="#modal" class="estudiantenombre"><i class="fa-solid fa-pencil"></i></a>
-                        Arias Catalina
-                    </td>
-
-                        <article id="modal" className="modal">
-                            <div class="modalContainer2">
-                                <div class="imagenestudiante">
-                                <i class="fa-solid fa-circle-user userss"></i>
-                                </div>
-                                <h2 class="nombre">Arias Catalina</h2>
-                                <h3 class="grado">9 Grado</h3>
-                                <div class="lista">
-                                <h4>Trabajos:</h4>
-                                <ul>
-                                    <li>Nombre trabajo1</li>
-                                    <li>Nombre trabajo2</li>
-                                    <li>Nombre trabajo3</li>
-                                </ul>
-                        
-                                </div>
-                                <div class="lista2">
-                                    <h4>notas:</h4>
-                                    <ul>
-                                        <li>7</li>
-                                        <li>5</li>
-                                        <li>10</li>
-                                    </ul>
-                                </div>
-                                
-
-                                <a href="">
-                                    <button class="btn-volver">Volver</button>
-                                    </a>
-                            </div>
-                            
-                        
-                        </article>
-                    
-                    <td class="email">catalinarias@colegiosanpatricio.com</td>
-                    <td class="workshop">
-                        
-                        <div class="worskhop-two">
-                        <a href="#modal2">
-                        <i class="fa-solid fa-paperclip"></i>Trabajo:Lorem ipsum
-                        </a>
-                        </div>
-                    <div class="worskshop-three">
-                            <a href="#modal2">
-                                <i class="fa-solid fa-paperclip"></i>Trabajo:Lorem ipsum
-                                </a>
-                        </div>
-                        <div class="workshop-four">
-                            <a href="#modal2">
-                                <i class="fa-solid fa-paperclip"></i>Trabajo:Lorem ipsum
-                                </a>
-                        </div>
-                    </td>
-                    <td class="score-students">
-                            <div>
-                            10
-                            </div>
-         
-                            <div>
-                            10
-                            </div>
-                            <div>
-                            10
-                            </div>
-                    </td>
-                    <td class="average">98%</td>
-                    </tr>
-                    <tr className="row-teacher"> 
-                    <td class="student"> 
-                        <a href="#modal" class="estudiantenombre"><i class="fa-solid fa-pencil"></i></a>
-                        Arias Catalina
-                    </td>
-
-                        <article id="modal" className="modal">
-                            <div class="modalContainer2">
-                                <div class="imagenestudiante">
-                                <i class="fa-solid fa-circle-user userss"></i>
-                                </div>
-                                <h2 class="nombre">Arias Catalina</h2>
-                                <h3 class="grado">9 Grado</h3>
-                                <div class="lista">
-                                <h4>Trabajos:</h4>
-                                <ul>
-                                    <li>Nombre trabajo1</li>
-                                    <li>Nombre trabajo2</li>
-                                    <li>Nombre trabajo3</li>
-                                </ul>
-                        
-                                </div>
-                                <div class="lista2">
-                                    <h4>notas:</h4>
-                                    <ul>
-                                        <li>7</li>
-                                        <li>5</li>
-                                        <li>10</li>
-                                    </ul>
-                                </div>
-                                
-
-                                <a href="">
-                                    <button class="btn-volver">Volver</button>
-                                    </a>
-                            </div>
-                            
-                        
-                        </article>
-                    
-                    <td class="email">catalinarias@colegiosanpatricio.com</td>
-                    <td class="workshop">
-                        
-                        <div class="worskhop-two">
-                        <a href="#modal2">
-                        <i class="fa-solid fa-paperclip"></i>Trabajo:Lorem ipsum
-                        </a>
-                        </div>
-                    <div class="worskshop-three">
-                            <a href="#modal2">
-                                <i class="fa-solid fa-paperclip"></i>Trabajo:Lorem ipsum
-                                </a>
-                        </div>
-                        <div class="workshop-four">
-                            <a href="#modal2">
-                                <i class="fa-solid fa-paperclip"></i>Trabajo:Lorem ipsum
-                                </a>
-                        </div>
-                    </td>
-                    <td class="score-students">
-                            <div>
-                            10
-                            </div>
-         
-                            <div>
-                            10
-                            </div>
-                            <div>
-                            10
-                            </div>
-                    </td>
-                    <td class="average">98%</td>
-                    </tr>
-                    <section id="modal2" class="modal2">
+                        <section id="modal2" class="modal2">
                         <div class="modalContainer3">
                             <h2>Nombre trabajo</h2>
                             <div class="descarga">
@@ -428,26 +93,12 @@ function ScoreT(){
                             <h2>Subido correctamente</h2>
                             <Link to="/">
                                 <button class="btn-volver">Volver</button>
-                                </Link>
-                        </div>
-                    </section>
-                    <section id="modal3" class="modal3">
-                        <div class="modalContainer4">
-                            <div class="stage">
-                                <div class="box bounce-6">
-                                    <i class="fa-solid fa-check-double check"></i>
-                                </div>
-                            </div>
-                            
-                            <h2>Subido correctamente</h2>
-                            <Link to="/">
-                                <button class="btn-volver">Volver</button>
                             </Link>
                         </div>
                     </section>
+
                     
-                    
-        </table>
+                
     </section>
 )
 }
--- src/teachers/scoreTeacher.js
+++ src/teachers/scoreTeacher.js
@@ -1,11 +1,13 @@
 import ScoreT from "../components/ScoreT/ScoreT"
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import CardScore from "../components/Tables/CardScoreT"
 function ScoreTeacher(){
     return(
         <section>
             <Header />
             <ScoreT />
+            <CardScore />
             <Footer />
 
         </section>
